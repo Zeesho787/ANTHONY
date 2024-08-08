a7470b7e4a76a32010b915cc6f6115afaadc4af5
Zeesho787 <125133907+Zeesho787@users.noreply.github.com>
2024-08-08 09:18:37 +0500

    Delete Auto.py

--- Auto.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
-os.system("clear")
-fbd=platform.architecture()[0]
-if fbd=="32bit":
-    __import__("FB-DEVIL32")
-elif fbd=="64bit":
-    __import__('BS') 
